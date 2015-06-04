714eb374a153994605a7b8076baeed721f9aef0a
Davide Eynard <davide.eynard@gmail.com>
2015-06-04 14:17:05 +0200

    Centered the Download link in the Chrome extension

--- chrome/moviESC.js
+++ chrome/moviESC.js
@@ -1,6 +1,7 @@
 var re = /\/tt(\d+)/; 
 var IMDBid = document.location.pathname.match(re)[1]; 
 var newDiv = document.createElement("div"); 
+newDiv.setAttribute("class","text-center")
 newDiv.innerHTML = '<a href="http://127.0.0.1:5000/api/v1.0/html/'+IMDBid+'">Download</a>'; 
 var dd = document.getElementsByClassName("pro-title-link")[0]; 
 dd.parentNode.insertBefore(newDiv,dd)
